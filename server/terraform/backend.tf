c5e1e35b38e1d6c0a42ba9df365f9b6dd9f59b6a
solufa <solufa2020@gmail.com>
2020-07-11 19:14:12 +0900

    chore: fix bucker name

--- server/terraform/backend.tf
+++ server/terraform/backend.tf
@@ -1,5 +1,5 @@
 resource aws_s3_bucket terraform_state {
-  bucket = terraform_state
+  bucket = "dgl-terraform-state"
   versioning {
     enabled = true
   }
@@ -9,6 +9,5 @@ terraform {
   backend s3 {
     bucket = aws_s3_bucket.terraform_state.terraform_state
     key    = "terraform.tfstate"
-    region = "ap-southeast-2"
   }
 }
