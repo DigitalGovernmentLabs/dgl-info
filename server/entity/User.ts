703fddc43b8090191122fd8bf9af9c343f86e8ae
Luma <world@luma.email>
2021-02-12 18:37:33 +0900

    feat: create schemas for auth

--- server/entity/User.ts
+++ server/entity/User.ts
@@ -1,16 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsAlphanumeric, MinLength } from "class-validator";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 128 })
+  @Column({ length: 128, unique: true })
+  @MinLength(2)
+  @IsAlphanumeric()
   name: string;
 
   @Column({ default: false })
   isAdmin: boolean;
 
-  @Column({ length: 256 })
+  @Column({ length: 256, select: false })
   passwordHash: string;
 }
