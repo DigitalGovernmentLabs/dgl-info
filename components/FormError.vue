2e66fb6453c025d13285c34ff1e37a7b4d9d7d46
Luma <world@luma.email>
2021-02-15 21:26:10 +0900

    feat(frnot/component): form error text style

---
+++ components/FormError.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-caption text-right font-weight-light red--text">
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: { message: { type: String, default: null } },
+});
+</script>
