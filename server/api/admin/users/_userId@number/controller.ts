306df922e373250fcdb4f40460fae9f402ac5cde
Luma <world@luma.email>
2021-02-15 21:35:48 +0900

    feat(api): implement users controllers

---
+++ server/api/admin/users/_userId@number/controller.ts
@@ -0,0 +1,81 @@
+import { getRepository } from "typeorm";
+import argon2 from "argon2";
+import { defineController } from "./$relay";
+import { User } from "$/entity/User";
+import { rules } from "$/service/form";
+
+const patchUserName = (user: User, userName: string) => {
+  if (!rules.isUserName(userName)) return { status: 400 } as const;
+  user.name = userName;
+};
+
+const patchPassword = async (user: User, password: string) => {
+  if (!rules.isPassword(password)) return { status: 400 } as const;
+  user.passwordHash = await argon2.hash(password, {
+    type: argon2.argon2id,
+  });
+};
+
+export default defineController(() => ({
+  get: async ({ params: { userId } }) => {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ id: userId });
+    if (user === undefined) return { status: 400 };
+    return {
+      status: 200,
+      body: {
+        id: user.id,
+        isAdmin: user.isAdmin,
+        name: user.name,
+      },
+    };
+  },
+  patch: async ({ body }) => {
+    const { userId } = body;
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ id: userId });
+    if (user === undefined)
+      return { status: 400, body: { errorMessage: "User not found." } };
+
+    // NOTE: schema をおいていないので任意の入力が来ることを想定し、能動的なチェックを行う。
+
+    if (typeof body.userName === "string") {
+      const err = patchUserName(user, body.userName);
+      if (err) return err;
+    }
+
+    if (typeof body.isAdmin === "boolean") {
+      user.isAdmin = body.isAdmin;
+    }
+
+    if (typeof body.password === "string") {
+      const err = await patchPassword(user, body.password);
+      if (err) return err;
+    }
+
+    await userRepository.save(user);
+    return {
+      status: 200,
+      body: {
+        id: user.id,
+        isAdmin: user.isAdmin,
+        name: user.name,
+      },
+    };
+  },
+  delete: async ({ body: { userId } }) => {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ id: userId });
+    if (user === undefined)
+      return { status: 400, body: { errorMessage: "User not found." } };
+    await userRepository.delete({ id: user.id });
+    return {
+      status: 200,
+      body: {
+        id: user.id,
+        isAdmin: user.isAdmin,
+        name: user.name,
+      },
+    };
+  },
+}));
---
+++ server/api/admin/users/controller.ts
@@ -0,0 +1,48 @@
+import { getRepository } from "typeorm";
+import argon2 from "argon2";
+import { defineController } from "./$relay";
+import { User } from "$/entity/User";
+import { rules } from "$/service/form";
+
+export default defineController(() => ({
+  get: async () => {
+    const userRepository = getRepository(User);
+    const users = (await userRepository.find({})).map((user) => ({
+      id: user.id,
+      name: user.name,
+      isAdmin: user.isAdmin,
+    }));
+    return { status: 200, body: users };
+  },
+  post: async ({ body: { userName, isAdmin, password } }) => {
+    // 入力値のチェック
+    if (rules.isUserName(userName) !== true) return { status: 400 };
+    if (rules.isPassword(password) !== true) return { status: 400 };
+
+    const userRepository = getRepository(User);
+    const exist =
+      (await userRepository.findOne({ name: userName })) !== undefined;
+    if (exist)
+      return {
+        status: 400,
+        body: { errorMessage: "すでに存在しているユーザ名です。" },
+      };
+    const passwordHash = await argon2.hash(password, {
+      type: argon2.argon2id,
+    });
+    const user = userRepository.create({
+      name: userName,
+      isAdmin,
+      passwordHash,
+    });
+    await userRepository.save(user);
+    return {
+      status: 200,
+      body: {
+        id: user.id,
+        name: user.name,
+        isAdmin: user.isAdmin,
+      },
+    };
+  },
+}));
