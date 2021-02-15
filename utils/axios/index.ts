4c17b7ec8302d78d669d41fb39ce799315e97fda
Luma <world@luma.email>
2021-02-15 21:27:57 +0900

    chore(front/utils): split axios error handler into one place

--- utils/axios/index.ts
+++ utils/axios/index.ts
@@ -3,3 +3,21 @@ import type { AxiosError } from "axios";
 export const isAxiosError = (e: unknown): e is AxiosError => {
   return Boolean((e as any).isAxiosError);
 };
+
+export const handleError = (e: unknown): string => {
+  // eslint-disable-next-line no-console
+  console.error(e);
+  if (isAxiosError(e)) {
+    const status = e.response?.status;
+    if (status === 400) {
+      const message = e.response?.data?.errorMessage;
+      if (typeof message === "string") return message;
+      return "入力が間違っています。";
+    } else if (status === 500) {
+      return "サーバーエラーが発生。";
+    } else {
+      return "通信エラーが発生。";
+    }
+  }
+  return "予期せぬエラーが発生。";
+};
