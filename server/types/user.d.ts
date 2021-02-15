d087410674864e24bbe285464910c207f2bf74a4
Luma <world@luma.email>
2021-02-15 21:18:11 +0900

    chore(security): change user typedef to increase security for the future

--- server/types/user.d.ts
+++ server/types/user.d.ts
@@ -1,5 +1,11 @@
-export type UserJwtPayload = {
+export type UserInfo = {
   id: number;
   name: string;
   isAdmin: boolean;
+
+  // SECURITY NOTE: user を渡すだけで楽するのは問題ないが、確実に明示的に消す。
+  password?: undefined;
+  passwordHash?: undefined;
 };
+
+export type UserJwtPayload = UserInfo;
