63c928825330c6e76edb782d8a8a466b701cd926
Luma <world@luma.email>
2021-03-05 18:07:55 +0900

    chore(api): update faq type defs

--- server/types/faq.d.ts
+++ server/types/faq.d.ts
@@ -2,15 +2,37 @@ import { Faq } from "$/entity/Faq";
 import { FaqGroup } from "$/entity/FaqGroup";
 import { DateISO8601 } from "$/utils/date";
 
+// Faq Group
+
+export type FaqGroupInfo = Pick<FaqGroup, "id" | "description" | "name"> & {
+  faqNumber: number;
+};
+
+export type FaqGroupCreate = Pick<FaqGroupInfo, "name"> &
+  Partial<Pick<FaqGroupInfo, "description">>;
+
+export type FaqGroupUpdate = Partial<
+  Pick<FaqGroupInfo, "name" | "description">
+>;
+
+// Faq
+
 export type FaqInfo = Pick<Faq, "id" | "question" | "answer" | "keywords"> & {
   groupId: FaqGroup["id"];
   createDate: DateISO8601 | null;
   updateDate: DateISO8601 | null;
 };
 
-export type FaqGroupInfo = Pick<FaqGroup, "id" | "description" | "name">;
+export type FaqCreate = Pick<FaqInfo, "question" | "answer" | "groupId">;
 
-export type FaqGroupCreate = Pick<FaqGroup, "name"> &
-  Partial<Pick<FaqGroup, "description">>;
+export type FaqUpdate = Partial<
+  Pick<FaqInfo, "question" | "answer" | "keywords">
+>;
 
-export type FaqGroupUpdate = Partial<Pick<FaqGroup, "name" | "description">>;
+export type FaqQuery = {
+  groupId: FaqGroupInfo["id"];
+};
+
+export type FaqQueryAnswer = {
+  faqList: FaqInfo[];
+};
